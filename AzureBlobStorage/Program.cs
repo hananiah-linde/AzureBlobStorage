ff4e4fd2962db89ac0b2e935be975b0901ad3c47
hananiah-linde <hananiah.linde@gmail.com>
2025-05-20 16:15:37 -0500

    initial commit

---
+++ AzureBlobStorage/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/reports", () =>
+    {
+
+    })
+    .WithName("DownloadReportFile");
+
+app.Run();
